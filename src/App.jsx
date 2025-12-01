4315f77b0345f5ebb1e3aae54de2a30f14d5fa08
karida-julia <karidajulia1@gmail.com>
2025-12-01 11:47:54 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import {useEffect, useState} from 'react'
+import s from '.App.module.css'
+import {api} from ".api/api"
+import {Card} from '.components/card'
+import './App.css'
+
+function App() {
+  const [data, setData] = useState([])
+  const [searchName, setSearchName] = useState([])
+  const [searchPage, setSearchPage] = useState([])
+
+  useEffect(()=>{
+     api.get(`/character/? 
+      name=${searchName}&page=${searchPage}`).then((response) => {
+          setData(response.data.results)
+        }).catch((error) => {
+          console.error("NÃO FOI POSSIVEL ACESSAR API", error)
+
+        })
+      }, [searchPage, searchName])
+  
+  return (
+    <>
+    <h1 className={s.title}>Disney</h1>
+    <main>
+      <div style={{display: "flex", flexWrap: "wrap", gap: "Spx", alignItems: "center", justifyContent: "center"}}>
+        <input type="text" value={searchPage}  onChange={(e) => setSearchPage(e.target.value)} placeholder='1/42'/>
+        <input type="text" value={searchName} onChange={(e) => setSearchName(e.target.value)} placeholder='Procure um personagem' />
+
+      </div>
+
+      <div className={s.wrapCards}
+      {data.map((item, index)}></div>
+    </main>
+    </>
+  )
+}
+
+export default App
